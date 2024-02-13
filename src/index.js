771cd8a8988fa28fc89434b3b90e1ee0c9ab825b
Shriganesh Hegde <shriganesh@ip-192-168-155-1.ap-southeast-1.compute.internal>
2024-02-13 07:24:39 +0530

    JSX, Ternary Element vs Component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const h1 = <h1>Hey there</h1>;
+const isMorning = true;
+
+const morningTxt = 'Hello good morning';
+const afternoonTxt = 'Hello good afternoon';
+
+const morningEle = <div>{morningTxt} <span>its 6AM now</span></div>
+const eveningEle = <div>{afternoonTxt}<span>its 6PM now</span></div>
+const greetingEle = <h1>{isMorning ? morningEle : eveningEle}</h1>
+
+const GreetingELement = ()=> {
+    return <div>
+        {greetingEle}
+        i am inside the component
+    </div>
+}
 const root = ReactDOM.createRoot(document.querySelector(".root"));
 
-root.render(h1);
+root.render(<GreetingELement />);
